116c27898ffa1a70521d2984b3a44b829f02d521
abdelrahmanghanam <60698814+abdelrahmanghanam@users.noreply.github.com>
2021-10-13 21:17:39 +0200

    fix

--- Main.kt
+++ Main.kt
@@ -1,21 +1,28 @@
 fun main(args: Array<String>)
 {
-    println("Hello world")
-    var age : Int =5
-    println(age.toString())
+    println("this is a program that determines acceptence in specific vacancy")
+
     var myand=And()
     var myios=IOS()
-    var myWeb=Web()
-    println("what is the name")
-    var name: String? = readLine()
-    println("what is the experience")
+//    //test case
+//    println(myios.offer("ahmed",2,24,2019))
+    var myweb=Web()
+    println("what is the name?")
+    var name: String = readLine().toString()
+    println("what are your experience years?")
+    var experience: String = readLine().toString()
+    println("what is your age?")
+    var age: String= readLine().toString()
+    println("what is the graduation year?")
+    var graduation: String= readLine().toString()
+    println("what vacancy are you applying for android: and, ios:ios, web:web?")
+    var vac: String= readLine().toString().lowercase()
+    when (vac){
+        "and" -> println(myand.offer(name,experience.toInt(),age.toInt(),graduation.toInt()))
+        "ios" ->println(myios.offer(name,experience.toInt(),age.toInt(),graduation.toInt()))
+        "web" ->println(myweb.offer(name,experience.toInt(),age.toInt(),graduation.toInt()))
+        else -> println(" wrong entry !!!")
+    }
 
-    var experience: String? = readLine()
-    println("what is the age")
-    var ages: String?= readLine()
-    println("what is the graduation year")
-    var graduation= readLine()
-//    println(myand.offer("ahmed",2,24,2019))
-    println(myand.offer(name,experience,age,graduation))
 
 }
